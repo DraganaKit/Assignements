5561ef42c42617cc5d89e301e86ecfd7227a6448
unknown <jevtic.s.dragana@gmail.com>
2024-12-15 11:06:32 +0100

    Dodat prvi zadatak

---
+++ Assignement.java
@@ -0,0 +1,12 @@
+public class Assignement
+{
+    public static void main(String[] args)
+    {
+    int i = 0;
+    while (i < 36) {
+        System.out.println("Hellow World!");
+        i++;
+    }
+    }
+}
+
